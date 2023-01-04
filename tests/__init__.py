478c8c5e0377acead6642eae093074f0eef5acf7
dkrajzew <d.krajzewicz@gmail.com>
2023-01-04 23:15:03 +0100

    adding some files; adding options parsing

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+"""docbook2qthelp.py
+
+!!!
+
+Init module
+
+(c) Daniel Krajzewicz 2020-2022
+daniel@krajzewicz.de
+http://www.krajzewicz.de
+https://github.com/dkrajzew/docbook2qthelp
+http://www.krajzewicz.de/blog/docbook2qthelp.php
+
+Available under LGPL 3 or later, all rights reserved
+"""
+from docbook2qthelp import *
